4a9cb29f090eb6e678d49f7ac3d750ee7b6ec81a
Tanner Welsh <tannerwelsh@gmail.com>
2016-11-21 12:41:54 -0500

    style: Install and configure ESLint
    Copied from ESLint config at https://github.com/GuildCrafts/floworky/.
    
    Other changes:
    - Add command `lint` to npm scripts
    - Clean up formatting in two files

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', routes)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found')
   err.status = 404
   next(err)
@@ -33,7 +33,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res) => {
     res.status(err.status || 500)
     res.render('error', {
       message: err.message,
@@ -44,7 +44,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   res.status(err.status || 500)
   res.render('error', {
     message: err.message,
