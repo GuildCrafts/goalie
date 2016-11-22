b4f6eb7ec7030c1c9ecb142b6656359a20e4cba1
Devon Wesley <dwesley89@gmail.com>
2016-11-22 09:26:05 -0800

    feat(authentication): initial install of github authentication/login
    Installed and fixed bugs on inital git hub log in setup. Can currently log in in a very basic way.
    
    2 WIP

--- config/authenticate.js
+++ config/authenticate.js
@@ -1,26 +1,27 @@
-// const passport = require('passport')
-// const GitHubStrategy = require('passport-github2').Strategy
-//
-// passport.serializeUser( ( user,done ) => {
-//   done( null, user )
-// })
-//
-// passport.deserializeUser( ( obj, done ) => {
-//   done( null, obj )
-// })
-//
-// const githubOptions = {
-//   clientID: 'f57f6ffc5c985b99c425',
-//   clientSecret: 'b64f20d41c339767a5e7fda2fe45acb7a41ac318',
-//   callbackURL: 'http://localhost:5000/auth/github/callback'
-// }
-//
-// passport.use( new GitHubStrategy( githubOptions ),
-//   ( accessToken, refreshToken, profile, done ) => {
-//     process.nextTick( () => done( null, profile ) )
-//   }
-// )
-//
-//
-//
-// module.exports = passport
+const passport = require('passport')
+const GitHubStrategy = require('passport-github2').Strategy
+
+var GITHUB_CLIENT_ID = "bccb7078d86ffdca72d5"
+var GITHUB_CLIENT_SECRET = "c35ef31927b43b0e34b5e92a5336735b9889d492"
+
+passport.serializeUser(function(user, done) {
+  done(null, user)
+})
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj)
+})
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/oauth_callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile)
+    })
+  }
+))
+
+module.exports = { passport }
