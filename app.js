73dbace22ce8fe948e14716ad7598d810ad708f7
Devon Wesley <dwesley89@gmail.com>
2016-11-21 17:46:56 -0800

    debuged github login/ need to refactor WIP

--- app.js
+++ app.js
@@ -6,6 +6,40 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const passport = require('passport')
+const methodOverride = require('method-override')
+const partials = require('express-partials')
+const util = require('util')
+const GitHubStrategy = require('passport-github2').Strategy
+
+
+var GITHUB_CLIENT_ID = "bccb7078d86ffdca72d5"
+var GITHUB_CLIENT_SECRET = "c35ef31927b43b0e34b5e92a5336735b9889d492"
+
+passport.serializeUser(function(user, done) {
+  done(null, user)
+})
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj)
+})
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/oauth_callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile)
+    })
+  }
+))
 
 const routes = require('./routes')
 
@@ -13,23 +47,65 @@ const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-
+app.set('view engine', 'ejs')
+app.use(partials())
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(methodOverride())
 app.use(express.static(path.join(__dirname, 'public')))
 
 // passport
-app.use(session({ 
+app.use(session({
   secret: 'OMG I LOVE SECRETS',
   resave: false,
   saveUninitialized: false
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user })
+})
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user })
+})
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user })
+})
+
+// GET /auth/github
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in GitHub authentication will involve redirecting
+//   the user to github.com.  After authorization, GitHub will redirect the user
+//   back to this application at /auth/github/callback
+app.get('/auth/github',
+  passport.authenticate('github', { scope: [ 'user:email' ] }),
+  function(req, res){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  })
+
+// GET /auth/github/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/oauth_callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/')
+  })
+
+app.get('/logout', function(req, res){
+  req.logout()
+  res.redirect('/')
+})
 
 // routes setup
 app.use('/', routes)
@@ -64,5 +140,10 @@ app.use((err, req, res) => {
     error: {}
   })
 })
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,18 @@ router.get('/', (req, res, next) => {
   res.render('index', {})
 })
 
-router.get( '/auth/github', 
-  passport.authenticate( 'github', { scope: [ 'user:email' ] } ),
-  (request, responst) => {
-    res.render('index', {})
-  }
-)
-
-router.get( '/auth/github/callback', 
-  passport.authenticat( 'github', { failureRedirect: '/login'} ),
-  ( request, response ) => {
-    response.redirect( '/' )
-  }
-)
+// router.get( '/auth/github', 
+//   passport.authenticate( 'github', { scope: [ 'user:email' ] } ),
+//   (request, responst) => {
+//     res.render('index', {})
+//   }
+// )
+//
+// router.get( '/auth/github/callback',
+//   passport.authenticat( 'github', { failureRedirect: '/login'} ),
+//   ( request, response ) => {
+//     response.redirect( '/' )
+//   }
+// )
 
 module.exports = router
