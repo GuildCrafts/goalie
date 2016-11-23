d05d581b7c78e0a6c37cb6d7fdec77332fa46ddc
Rachel Ralston <rachelralston@gmail.com>
2016-11-22 17:11:32 -0800

    refactor(browser): refactoring syntax & details
    2

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ app.use('/', routes)
 app.use('/auth', auth)
 
 // catch 404 and forward to error handler
-app.use( ( req, res, next ) => {
-  const err = new Error( 'Not Found' )
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
   err.status = 404
   next(err)
 })
@@ -50,10 +50,10 @@ app.use( ( req, res, next ) => {
 
 // development error handler
 // will print stacktrace
-if ( app.get( 'env' ) === 'development' ) {
-  app.use( ( err, req, res ) => {
-    res.status( err.status || 500 )
-    res.render( 'error', {
+if (app.get('env') === 'development') {
+  app.use((err, req, res) => {
+    res.status(err.status || 500)
+    res.render('error', {
       message: err.message,
       error: err
     })
@@ -62,9 +62,9 @@ if ( app.get( 'env' ) === 'development' ) {
 
 // production error handler
 // no stacktraces leaked to user
-app.use( ( err, req, res ) => {
-  res.status( err.status || 500 )
-  res.render( 'error', {
+app.use((err, req, res) => {
+  res.status(err.status || 500)
+  res.render('error', {
     message: err.message,
     error: {}
   })
--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,23 @@
-const express = require( 'express' )
+const express = require('express')
 const router = express.Router()
-const { passport } = require( '../config/authenticate' )
+const { passport } = require('../config/authenticate')
 
-router.get( '/login',
-  passport.authenticate( 'github', { scope: [ 'user:email' ] })
+router.get('/login',
+  passport.authenticate('github', {scope:[ 'user:email' ]})
 )
 
 router.get('/oauth_callback',
-  passport.authenticate( 'github', { failureRedirect: '/login' }),
-  ( req, res ) => {
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  (req, res) => {
     const { code } = req.query
     const { user } = req
-    console.log('========',user)
-    res.status( 200 ).render( 'list', { user })
+    res.status(200).render('list', { user })
   }
 )
 
-router.get( '/logout', ( req, res ) => {
+router.get('/logout', (req, res) => {
   req.logout()
-  res.status( 200 ).json( {} )
+  res.status(200).json( {} )
 })
 
 module.exports = router
