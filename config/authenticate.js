abc9d4d1c4b4595ddd024fef5a92ef84e34b96ca
Rachel Ralston <rachelralston@gmail.com>
2016-11-22 17:11:32 -0800

    refactor(browser): refactoring syntax & details
    2

--- config/authenticate.js
+++ config/authenticate.js
@@ -6,11 +6,10 @@ const GitHubCallback = (accessToken, refreshToken, profile, done) =>
 passport.serializeUser((user, done) => done(null, user))
 passport.deserializeUser((obj, done) => done(null, obj))
 
-
 passport.use( new GitHubStrategy( {
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/oauth_callback"
+    callbackURL: process.env.APP_URL + "/auth/oauth_callback"
   },
   GitHubCallback
 ))
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require( 'express' )
 const router = express.Router()
 const passport = require( '../config/authenticate' )
 
-router.get( '/', ( req, res, next ) => {
-  res.render( 'index', {} )
-})
+router.get('/', (req, res, next) => res.render('index', {}))
 
 module.exports = router
