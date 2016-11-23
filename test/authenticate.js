f9707b89d10f10b772942cea9b7b2a1577fb3178
Devon Wesley <dwesley89@gmail.com>
2016-11-23 11:11:22 -0800

    test(test): mergeds the test into one, Still need to figure out how to test against githubs api WIP
    Test compacted together to form one test, need to figure out how to test when app is athorized by
    
    github user.

--- test/authenticate.js
+++ test/authenticate.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require('dotenv').config()
 const chai = require('chai')
 const chaiHTTP = require('chai-http')
-
 const server = require('../app')
 
+const {expect} = chai
 const should = chai.should()
 
 chai.use(chaiHTTP)
@@ -12,27 +12,23 @@ describe('Auth: ', () => {
   describe('/auth/login route', () => {
     it('redirects to github for log in', (done) => {
       chai.request(server)
-      .get('/auth/login')
-      .end((err, response) => {
-        response.should.have.status(200)
-        response.headers.should.have.property('server').eql('GitHub.com')
-        done()
-      })
-    });
-  })
-
-  describe('/auth/oauth_callback route', () => {
-    it('redirects to github for log in', (done) => {
-      chai.request(server)
-      .get('/auth/login')
-      .end((err, response) => {
-        // should return json/object
-        // object should have user section
-        // code should be in the query
-        // user should have access to @GuildCrafts
-        response.should.have.status(200)
-        done()
-      })
+        .get('/auth/login')
+        .end((error, response) => {
+          expect(response).to.redirect
+          response.should.have.status(200)
+          response.body.should.be.a('object')
+          response.headers.should.have.property('server').eql('GitHub.com')
+          response.redirects.should.be.a('array')
+          done()
+        })
+        .then(callbackUrl => {
+          chai.request(callbackUrl.redirects[0])
+            .get('/')
+            .end((error, response) => {
+              response.should.have.status(200)
+              done()
+            })
+        })
     });
   })
 })
