746be16bfcd802952be0da0c676619c29d5cae98
Devon Wesley <dwesley89@gmail.com>
2016-12-03 13:06:19 -0800

    General refactoring of code and file organization check, deleted unnecessary packages
    Reorganize app.js file so that webpack doesn't interfere with testing
    
    reabsed from master to have masters changes and reorganize app.js file so routes are above the webpack middleware
    
    test(testing): created test for githubapi requests
    
    created testing that intercepts outgoing requests to githubapi. and redirects to a mock github
    
    listener that responses with mock github object
    
    Testing implemented
    
    refactor(test): got first test to pass
    
    still need to finish writing second test, but first test is passing
    
    2
    
    refactor(browser): refactoring syntax & details
    
    2
    
    refactor(browser): basic ui
    
    2
    
    refactor(browser): updating basic views
    
    2
    
    refactor(refactor auth): refactoring PR comments
    
    2
    
    fix(security): update .env
    
    updated .env to take out details/add to gitignore. Updated 'Development Setup' section of readme to
    
    include that information.
    
    2
    
    test(set up): setting up testing suite
    
    write first passing test for routes using mocha/chai
    
    fix(authentication): updating spacing in reply to PR
    
    2
    
    refactor(debug): authorization: updating bull request
    
    2
    
    refactor(authentication): refactored basic auth
    
    refactored for es6 and so that everything works well
    
    2 WIP
    
    feat(authentication): initial install of github authentication/login
    
    Installed and fixed bugs on inital git hub log in setup. Can currently log in in a very basic way.
    
    2 WIP
    
    debuged github login/ need to refactor WIP

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require( 'express' )
+const router = express.Router()
+const {passport} = require('../config/authenticate')
+const {authCallback, logOutCallback} = require('./authenticate/authMethods')
+
+router.get(
+  '/login',
+  passport.authenticate('github', {scope:['user:email']})
+)
+
+router.get(
+  '/oauth_callback',
+  passport.authenticate('github', {failureRedirect: '/login'}),
+  authCallback
+)
+
+router.get('/logout', logOutCallback)
+
+module.exports = router
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require( 'express' )
+const router = express.Router()
+const passport = require( '../config/authenticate' )
+
+router.get('/', (req, res, next) => res.render('index', {}))
+
+module.exports = router
